0d83cfbff4dda64f974d6c8b00cf7d03c735de11
kabiri95 <erfankabiri337@gmail.com>
2022-05-09 16:07:46 +0430

    create todo

--- A/home/views.py
+++ A/home/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .models import Todo
 from django.contrib import messages
+from .forms import TodoCreateForm
 
 def home(request):
     all = Todo.objects.all()
-    return render(request, 'home.html', context= {'todos':all})
+    return render(request, 'home.html', context={'todos': all})
 
 
 def detail(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
-    return render(request, 'detail.html', {'todo':todo})
+    return render(request, 'detail.html', {'todo': todo})
 
 def delete(request, todo_id):
     Todo.objects.get(id=todo_id).delete()
     messages.success(request, 'todo deleted successfully', extra_tags='success')
     return redirect('home')
+
+
+def create(request):
+    form =TodoCreateForm()
+    return render(request, 'create.html', {'form': form})

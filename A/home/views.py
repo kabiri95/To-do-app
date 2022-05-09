1d856bbdf5a558f7093a525d29c34e4239b00154
kabiri95 <erfankabiri337@gmail.com>
2022-05-09 14:52:17 +0430

    add message

--- A/home/views.py
+++ A/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
+from django.contrib import messages
 
 def home(request):
     all = Todo.objects.all()
@@ -13,4 +14,5 @@ def detail(request, todo_id):
 
 def delete(request, todo_id):
     Todo.objects.get(id=todo_id).delete()
+    messages.success(request, 'todo deleted successfully', extra_tags='success')
     return redirect('home')

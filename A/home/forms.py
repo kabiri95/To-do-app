73a9dfc5f664778c5aec24bae64133f2f98ad1b2
kabiri95 <erfankabiri337@gmail.com>
2022-05-14 14:20:09 +0430

    update form

--- A/home/forms.py
+++ A/home/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Todo
 
 
 class TodoCreateForm(forms.Form):
@@ -6,3 +7,9 @@ class TodoCreateForm(forms.Form):
     body = forms.CharField()
     created = forms.DateTimeField()
     deadline = forms.DateTimeField(required=False)
+
+class TodoUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
--- A/home/views.py
+++ A/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Todo
 from django.contrib import messages
-from .forms import TodoCreateForm
+from .forms import TodoCreateForm, TodoUpdateForm
 
 def home(request):
     all = Todo.objects.all()
@@ -29,3 +29,17 @@ def create(request):
     else:
         form =TodoCreateForm()
     return render(request, 'create.html', {'form': form})
+
+
+def update(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    if request.method == 'POST':
+        form =TodoUpdateForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'your todo update successfully', 'success')
+            return redirect('details', todo_id)
+    else:
+        form = TodoUpdateForm(instance=todo)
+    return render(request, 'update.html', {'form': form})
+
